a8dabc87b59093c26241eccae802d70070a23d1e
Eleri <elviks@ttu.ee>
2019-12-10 15:28:07 +0200

    ´ to ` SELECT's

--- db/h01.php
+++ db/h01.php
@@ -11,7 +11,7 @@ echo '<pre>';
 print_r($ikt);
 
 //$slq = 'RENAME TABLE `vikseler_anna.php`.`TABLE 1` TO `vikseler_anna.php`.`koolid2015`';
-$sql = 'SELECT Kool, Kokku FROM koolid2015';
+$sql = `SELECT Kool, Kokku FROM koolid2015`;
 $result = query($sql, $ikt);
 echo '<pre>';
 print_r($result);
