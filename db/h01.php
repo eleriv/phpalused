afdf04a378de7c272b780efcdb1a2afc0dfff8d5
Eleri <elviks@ttu.ee>
2019-12-10 15:24:30 +0200

    tabel vahetas nime

--- db/h01.php
+++ db/h01.php
@@ -10,8 +10,8 @@ $ikt = connection(HOST, USER, USERPASS, DBNAME);
 echo '<pre>';
 print_r($ikt);
 
-$slq = 'RENAME TABLE `vikseler_anna.php`.`TABLE 1` TO `vikseler_anna.php`.`koolid2015`';
-$sql = 'SELECT * FROM koolid';
-$result = query($slq, $ikt);
+//$slq = 'RENAME TABLE `vikseler_anna.php`.`TABLE 1` TO `vikseler_anna.php`.`koolid2015`';
+$sql = 'SELECT * FROM koolid2015';
+$result = query($sql, $ikt);
 echo '<pre>';
 print_r($result);
