8be023701df871119175bcb217ed8dc9c6db31a6
Eleri <elviks@ttu.ee>
2019-12-10 14:54:48 +0200

    KATSE

---
+++ db/h01.php
@@ -0,0 +1,14 @@
+<?php
+//lisame andmebaasitöötlus funktsioonid
+require_once 'config.php';
+//lisame kasutusele andmebaasi serveri konfiguratsiooni
+require_once 'db_fnc.php';
+// ühendus andmebaasiga
+$ikt = connection(HOST, USER, USERPASS, DBNAME);
+// katsetame sql
+
+echo '<pre>';
+print_r($ikt);
+
+$slq = 'RENAME TABLE `vikseler_anna.php`.`TABLE 1 TO `vikseler_anna.php`.`koolid2015`';
+$result = query($slq, $ikt);
