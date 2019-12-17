b9424a57b397467b604aa0f4ddc8dd3ec2f38ab3
Eleri <elviks@ttu.ee>
2019-12-17 09:37:19 +0200

    proov

--- db/db_fnc.php
+++ db/db_fnc.php
@@ -1,35 +1,33 @@
 <?php
-// loome ühendus
-function connection($host, $user, $pass, $dbname)
-{
+// ühenduse loomine
+function connect($host, $user, $pass, $dbname){
     $link = mysqli_connect($host, $user, $pass, $dbname);
-    // kui ühendust ei tekkinud
-    if ($link === false) {
-        // veateade probleemi korral
-        echo 'Probleem andmebaasi ühendamisega!<br>';
+    if($link === false){
+        echo 'Probleem andmebaasi ühendusega<br>';
         exit;
     }
-    // tagastame edaspidiseks kasutamiseks
+    mysqli_set_charset($link, 'utf8');
+
     return $link;
 }
-
 
 // suvalise päringu saatmine andmebaasi
 function query($sql, $link){
     $result = mysqli_query($link, $sql);
     if($result === false){
         echo 'Probleem päringuga <b>'.$sql.'</b><br>';
+        return false;
     }
-    //kui $result on true
     return $result;
 }
+
 function getData($sql, $link){
     $result = query($sql, $link);
     $data = array();
-    // while- loeb kõik andmed ükshaaval
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
         $data[] = $row;
     }
+
     if(count($data) == 0){
         return false;
     }
